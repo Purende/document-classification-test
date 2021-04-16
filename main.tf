8b781c767e795413f86f161016664e88f7eb7f72
Purende <39925054+Purende@users.noreply.github.com>
2021-04-15 22:36:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-  profile = "saml"
-  region  = "${var.region_name}"
- 
+  region  = "${var.region_name}" 
 }
 
 ### ALB
-resource "aws_alb" "main" {
+resource "aws_alb" "main1" {
   name            = "${var.alb_name}"
   internal        = true 
   subnets         = ["subnet-3d4d7270", "subnet-7da2c85c"]
@@ -22,30 +20,16 @@ resource "aws_alb_target_group" "app" {
 
 # Redirect all traffic from the ALB to the target group
 resource "aws_alb_listener" "front_end" {
-  load_balancer_arn = "${aws_alb.main.id}"
+  load_balancer_arn = "${aws_alb.main1.id}"
   port              = "80"
   protocol          = "HTTP"
-
   default_action {
     target_group_arn = "${aws_alb_target_group.app.id}"
     type             = "forward"
   }
  }
-# resource "aws_alb_listener" "front" {
-#   load_balancer_arn = "${aws_alb.main.id}"
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   certificate_arn   = "arn:aws:acm:us-east-1:302939895826:certificate/ds51f99f-gh11-458g-9vgt-08654b3935b5343"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08" 
-
-#   default_action {
-#     target_group_arn = "${aws_alb_target_group.app.id}"
-#     type             = "forward"
-#   }
-#  }
 
  ### ECS Cluster for DCM
-
 resource "aws_ecs_cluster" "DCM-MLOPS" {
   name = "${var.ecs_cluster}"
 }
@@ -54,15 +38,14 @@ resource "aws_ecs_task_definition" "DCM-MLOPS-APP" {
   family                   = "${var.ecs_task_definition}"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 2048
-  memory                   = 3000
+  cpu                      = 1024
+  memory                   = 3072
   task_role_arn            = "${var.ecs_role}"  
   execution_role_arn       = "${var.ecs_role}"               
-  container_definitions = <<DEFINITION
-[
+  container_definitions = jsonencode([
   {
     "image": "302939895826.dkr.ecr.us-east-1.amazonaws.com/dcm-deploy:latest",
-    "memoryReservation": 2000,
+    "memoryReservation": 2048,
     "name": "app",
     "networkMode": "awsvpc",
     "portMappings": [
@@ -80,8 +63,7 @@ resource "aws_ecs_task_definition" "DCM-MLOPS-APP" {
       }
     }
   }
-]
-DEFINITION
+  ])
 }
 
 resource "aws_ecs_service" "main" {
